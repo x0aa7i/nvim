16e0a23efe2674958e4fa955215df9749f79e3a8
ABDO <lonihilist@gmail.com>
2024-05-23 17:33:07 +0100

    update lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,22 +1,24 @@
 return {
   "nvim-lualine/lualine.nvim",
   event = "VeryLazy",
-  opts = {
-    options = {
-      icons_enabled = true,
-      component_separators = "",
-      -- section_separators = "",
-    },
-    sections = {
-      lualine_a = {
-        { "mode", icon = { "" } },
-      },
-      lualine_b = {},
-      lualine_y = {},
-      lualine_z = {
-        { "progress", separator = " ", padding = { left = 1, right = 0 } },
-        { "location", padding = { left = 0, right = 1 } },
-      },
-    },
-  },
+  opts = function(_, opts)
+    local function total_lines()
+      return "󰦨 " .. vim.api.nvim_buf_line_count(0)
+    end
+
+    opts.options.component_separators = ""
+
+    opts.sections.lualine_a = { { "mode", icon = { "" } } }
+    opts.sections.lualine_b = {}
+
+    table.insert(opts.sections.lualine_c, { "filesize", color = { fg = "#737aa2" } })
+    table.insert(opts.sections.lualine_x, { total_lines, color = { fg = "#a9b1d6" } })
+
+    opts.sections.lualine_y = {}
+    opts.sections.lualine_z = {
+      -- location replaced with scrollbar
+      -- { "progress", padding = { left = 1, right = 0 } },
+      { "location", padding = { left = 1, right = 1 } },
+    }
+  end,
 }
