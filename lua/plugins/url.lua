33d1c6fa14915c401adf2c308cb09c7664393bb1
ABDO <lonihilist@gmail.com>
2024-06-01 12:29:22 +0100

    feat(url): add support for package.json dependencies

--- lua/plugins/url.lua
+++ lua/plugins/url.lua
@@ -12,6 +12,19 @@ return {
     },
   },
   config = function()
-    require("url-open").setup({})
+    require("url-open").setup({
+      extra_patterns = {
+        {
+          pattern = '["]([^%s]*)["]:%s*"[^"]*%d[%d%.]*"',
+          prefix = "https://www.npmjs.com/package/",
+          suffix = "",
+          file_patterns = { "package%.json" },
+          excluded_file_patterns = nil,
+          extra_condition = function(pattern_found)
+            return not vim.tbl_contains({ "version", "proxy" }, pattern_found)
+          end,
+        },
+      },
+    })
   end,
 }
