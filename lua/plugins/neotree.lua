95eadad28344a74d8630da300d7d6c71aa0dfd14
ABDO <lonihilist@gmail.com>
2024-06-30 18:33:28 +0100

    refactor: neo-tree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,69 +1,93 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    -- close_if_last_window = true,
-    window = {
-      position = "float",
-      width = 70,
-      mappings = {
-        -- ["<space>"] = "none",
-        ["<space>"] = {
-          "toggle_node",
-          nowait = true, -- disable `nowait` if you have existing combos starting with this char that you want to use
-        },
-      },
-    },
-    filesystem = {
-      bind_to_cwd = false,
-      follow_current_file = { enabled = true },
-      use_libuv_file_watcher = true,
-      filtered_items = {
-        hide_dotfiles = false,
-        hide_hidden = false,
-        hide_gitignored = false,
-      },
-      commands = {
-        -- overwrite default 'delete' command to 'trash'.
-        -- https://github.com/nvim-neo-tree/neo-tree.nvim/issues/202
-        delete = function(state)
-          local inputs = require("neo-tree.ui.inputs")
-          local path = state.tree:get_node().path
-          local msg = "Are you sure you want to trash " .. path
-          inputs.confirm(msg, function(confirmed)
-            if not confirmed then
-              return
-            end
+  opts = function(_, opts)
+    opts.close_if_last_window = true
+    opts.hide_root_node = true
+    opts.retain_hidden_root_indent = true
 
-            vim.fn.system({ "trash", vim.fn.fnameescape(path) })
-            require("neo-tree.sources.manager").refresh(state.name)
-          end)
-        end,
-        -- over write default 'delete_visual' command to 'trash' x n.
-        delete_visual = function(state, selected_nodes)
-          local inputs = require("neo-tree.ui.inputs")
+    opts.window = opts.window or {}
+    opts.window.position = "float"
 
-          -- get table items count
-          function GetTableLen(tbl)
-            local len = 0
-            for n in pairs(tbl) do
-              len = len + 1
-            end
-            return len
-          end
+    opts.window.mappings = opts.window.mappings or {}
+    opts.window.mappings["<space>"] = {
+      "toggle_node",
+      nowait = true, -- disable `nowait` if you have existing combos starting with this char that you want to use
+    }
 
-          local count = GetTableLen(selected_nodes)
-          local msg = "Are you sure you want to trash " .. count .. " files ?"
-          inputs.confirm(msg, function(confirmed)
-            if not confirmed then
-              return
-            end
-            for _, node in ipairs(selected_nodes) do
-              vim.fn.system({ "trash-put", vim.fn.fnameescape(node.path) })
-            end
-            require("neo-tree.sources.manager").refresh(state.name)
-          end)
-        end,
-      },
-    },
-  },
+    -- Show hidden files
+    opts.filesystem = opts.filesystem or {}
+    opts.filesystem.filtered_items = opts.filesystem.filtered_items or {}
+    opts.filesystem.filtered_items.hide_dotfiles = false
+    opts.filesystem.filtered_items.hide_gitignored = false
+    opts.filesystem.filtered_items.hide_hidden = false
+
+    -- Default component configs
+    opts.default_component_configs = opts.default_component_configs or {}
+
+    opts.default_component_configs.name = opts.default_component_configs.name or {}
+    opts.default_component_configs.name.highlight_opened_files = true
+
+    opts.default_component_configs.modified = opts.default_component_configs.modified or {}
+    opts.default_component_configs.modified.symbol = ""
+    opts.default_component_configs.modified.highlight = "NeoTreeModified"
+
+    opts.default_component_configs.git_status = opts.default_component_configs.git_status or {}
+    opts.default_component_configs.git_status.symbols = opts.default_component_configs.git_status.symbols or {}
+    -- Change git status type
+    opts.default_component_configs.git_status.symbols.added = "" -- or  "✚", but this is redundant info if you use git_status_colors on the name
+    opts.default_component_configs.git_status.symbols.modified = "" -- or 󰧞 "", but this is redundant info if you use git_status_colors on the name
+    opts.default_component_configs.git_status.symbols.deleted = "" -- this can only be used in the git_status source
+    opts.default_component_configs.git_status.symbols.renamed = "" -- this can only be used in the git_status source
+
+    opts.default_component_configs.git_status.symbols.untracked = "?" -- 
+    opts.default_component_configs.git_status.symbols.ignored = ""
+    opts.default_component_configs.git_status.symbols.unstaged = ""
+    opts.default_component_configs.git_status.symbols.staged = ""
+    opts.default_component_configs.git_status.symbols.conflict = ""
+
+    opts.default_component_configs.filesystem = opts.default_component_configs.filesystem or {}
+    opts.default_component_configs.filesystem.commands = opts.default_component_configs.filesystem.commands or {}
+
+    local inputs = require("neo-tree.ui.inputs")
+    local cmds = require("neo-tree.sources.filesystem.commands")
+
+    -- over write default 'delete' command to 'trash'.
+    opts.default_component_configs.filesystem.commands.delete = function(state)
+      local utils = require("neo-tree.utils")
+
+      local node = state.tree:get_node()
+      if node.type == "message" then
+        return
+      end
+      local _, name = utils.split_path(node.path)
+      local msg = string.format("Are you sure you want to trash '%s'?", name)
+      inputs.confirm(msg, function(confirmed)
+        if not confirmed then
+          return
+        end
+        vim.api.nvim_command("silent !trash -f " .. node.path)
+        cmds.refresh(state)
+      end)
+    end
+
+    -- over write default 'delete_visual' command to 'trash' x n.
+    opts.default_component_configs.filesystem.commands.delete_visual = function(state, selected_nodes)
+      local paths_to_trash = {}
+      for _, node in ipairs(selected_nodes) do
+        if node.type ~= "message" then
+          table.insert(paths_to_trash, node.path)
+        end
+      end
+      local msg = "Are you sure you want to trash " .. #paths_to_trash .. " items?"
+      inputs.confirm(msg, function(confirmed)
+        if not confirmed then
+          return
+        end
+        for _, path in ipairs(paths_to_trash) do
+          vim.api.nvim_command("silent !trash -f " .. path)
+        end
+        cmds.refresh(state)
+      end)
+    end
+  end,
 }
