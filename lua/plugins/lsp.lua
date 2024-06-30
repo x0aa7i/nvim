4e43dbaf71f502ca0f3e9b1d422cd96cc9afcf5d
ABDO <lonihilist@gmail.com>
2024-06-30 18:54:25 +0100

    refactor: lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,57 +1,78 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      -- diagnostics = {
-      --   signs = false,
-      -- },
-      flags = {
-        debounce_text_changes = 200,
-        allow_incremental_sync = true,
-      },
-      servers = {
-        ----@type lspconfig.options.svelte
-        svelte = {
-          capabilities = {
-            workspace = {
-              didChangeWatchedFiles = {
-                dynamicRegistration = true,
-              },
-            },
-          },
-        },
-        ----@type lspconfig.options.tailwindcss
-        tailwindcss = {
-          settings = {
-            tailwindCSS = {
-              experimental = {
-                classRegex = {
-                  -- Custom class name attributes (e.g. buttonClassName)
-                  { [==[[a-zA-Z]*ClassName=["'`]([^"'`]+)["'`]]==] },
-                  -- cls, clsx
-                  -- https://github.com/tailwindlabs/tailwindcss-intellisense/issues/682#issuecomment-1364585313
-                  {
-                    [[cn\(([^)(]*(?:\([^)(]*(?:\([^)(]*(?:\([^)(]*\)[^)(]*)*\)[^)(]*)*\)[^)(]*)*)\)]],
-                    '"(.*?)"',
-                  },
-                  -- Tailwind Variants
-                  -- https://www.tailwind-variants.org/docs/getting-started#intellisense-setup-optional
-                  {
-                    [[tv\(([^)(]*(?:\([^)(]*(?:\([^)(]*(?:\([^)(]*\)[^)(]*)*\)[^)(]*)*\)[^)(]*)*)\)]],
-                    '"(.*?)"',
-                  },
-                  -- `styles` objects
-                  -- https://github.com/tailwindlabs/tailwindcss-intellisense/issues/682#issuecomment-1364585313
-                  { [[styles =([^}]*)\}]], [==[["'`]([^"'`]*).*?["'`]]==] },
-                },
-              },
-            },
-          },
-        },
-      },
-      -- you can do any additional lsp server setup here
-      -- return true if you don't want this server to be setup with lspconfig
+    dependencies = {
+      -- { "artemave/workspace-diagnostics.nvim", opts = {} },
     },
+    opts = function(_, opts)
+      opts.servers = opts.servers or {}
+      ----@type lspconfig.options.tailwindcss
+      opts.servers.tailwindcss = {
+        settings = {
+          tailwindCSS = {
+            experimental = {
+              classRegex = {
+                -- Custom class name attributes (e.g. buttonClassName)
+                { [==[[a-zA-Z]*ClassName=["'`]([^"'`]+)["'`]]==] },
+                -- cls, clsx
+                -- https://github.com/tailwindlabs/tailwindcss-intellisense/issues/682#issuecomment-1364585313
+                {
+                  [[cn\(([^)(]*(?:\([^)(]*(?:\([^)(]*(?:\([^)(]*\)[^)(]*)*\)[^)(]*)*\)[^)(]*)*)\)]],
+                  '"(.*?)"',
+                },
+                -- Tailwind Variants
+                -- https://www.tailwind-variants.org/docs/getting-started#intellisense-setup-optional
+                {
+                  [[tv\(([^)(]*(?:\([^)(]*(?:\([^)(]*(?:\([^)(]*\)[^)(]*)*\)[^)(]*)*\)[^)(]*)*)\)]],
+                  '"(.*?)"',
+                },
+                -- `styles` objects
+                -- https://github.com/tailwindlabs/tailwindcss-intellisense/issues/682#issuecomment-1364585313
+                { [[styles =([^}]*)\}]], [==[["'`]([^"'`]*).*?["'`]]==] },
+              },
+            },
+          },
+        },
+      }
+
+      -- Capabilities
+      opts.capabilities = {
+        workspace = {
+          -- PERF: didChangeWatchedFiles is too slow.
+          -- TODO: Remove this when https://github.com/neovim/neovim/issues/23291#issuecomment-1686709265 is fixed.
+          didChangeWatchedFiles = { dynamicRegistration = false },
+        },
+      }
+
+      -- Inlay hints
+      opts.inlay_hints = opts.inlay_hints or {}
+      opts.inlay_hints.enabled = false
+
+      -- Diagnostics
+      opts.diagnostics = opts.diagnostics or {}
+      opts.diagnostics.virtual_text = opts.diagnostics.virtual_text or {}
+      opts.diagnostics.virtual_text.prefix = "icons"
+
+      -- Code lens
+      opts.codelens = opts.codelens or {}
+      opts.codelens.enabled = false
+
+      -- Semantic tokens
+      LazyVim.lsp.on_attach(function(client, buffer)
+        if
+          vim.lsp.semantic_tokens
+          and client.supports_method("textDocument/semanticTokens/full")
+          and vim.b[buffer].semantic_tokens == nil
+        then
+          vim.b[buffer].semantic_tokens = true
+        end
+      end)
+
+      -- Workspace diagnostics
+      -- LazyVim.lsp.on_attach(function(client, buffer)
+      --   require("workspace-diagnostics").populate_workspace_diagnostics(client, buffer)
+      -- end)
+    end,
   },
   {
     "nvim-treesitter/nvim-treesitter",
@@ -94,11 +115,7 @@ return {
         "stylelint-lsp", -- css linter
         "emmet-language-server", -- css/html completions
         "lua-language-server",
-        "marksman", -- markdown lsp
-        "stylelint-lsp",
         "taplo", -- toml lsp
-        "typescript-language-server",
-        "typos-lsp", -- spellchecker for code
       },
     },
   },
