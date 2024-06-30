9bb83a7a614280ce88fb7c966c8f88a74b034801
ABDO <lonihilist@gmail.com>
2024-06-30 12:45:58 +0100

    refactor: noice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,57 +1,42 @@
 return {
   "folke/noice.nvim",
   opts = function(_, opts)
-    opts.presets = vim.tbl_extend("force", opts.presets, {
-      lsp_doc_border = true, -- add a border to hover docs and signature help
-    })
+    opts.presets = opts.presets or {}
+    opts.presets.lsp_doc_border = true -- add a border to hover docs and signature help
 
-    opts.routes = vim.tbl_extend("force", opts.routes, {
-      -- write/deletion messages
-      { filter = { event = "msg_show", find = "%d+B written$" }, view = "mini" },
-      { filter = { event = "msg_show", find = "%d+L, %d+B$" }, view = "mini" },
-      { filter = { event = "msg_show", find = "%-%-No lines in buffer%-%-" }, view = "mini" },
-      -- Diagnostics
-      { filter = { event = "msg_show", find = "No more valid diagnostics to move to" }, view = "mini" },
-      -- nvim-treesitter
-      { filter = { event = "msg_show", find = "^%[nvim%-treesitter%]" }, view = "mini" },
-      { filter = { event = "notify", find = "All parsers are up%-to%-date" }, view = "mini" },
-      -- LSP
+    vim.list_extend(opts.routes, {
       {
-        filter = { event = "notify", find = "No information available" },
-        view = "mini",
-        opts = { lang = "svelte", skip = true },
-      },
-      -- Supermaven
-      {
+        -- Disable messages
+        opts = { skip = true },
         filter = {
-          event = "msg_show",
           any = {
-            { find = "Starting Supermaven" },
-            { find = "Supermaven Free Tier" },
+            { event = "msg_show", find = "%d+ lines, %d+ bytes" },
+            { event = "msg_show", find = "%d+L, %d+B" },
+            { event = "msg_show", find = "^Hunk %d+ of %d" },
+            { event = "msg_show", find = "%d+ change" },
+            { event = "msg_show", find = "%d+ line" },
+            { event = "msg_show", find = "%d+ more line" },
           },
         },
-        skip = true,
+      },
+      {
+        -- Mini view
+        view = "mini",
+        filter = {
+          any = {
+            { event = "msg_show", find = "%d+B written$" },
+            { event = "msg_show", find = "%-%-No lines in buffer%-%-" },
+            { event = "msg_show", find = "No more valid diagnostics to move to" },
+            { event = "msg_show", find = "^%[nvim%-treesitter%]" },
+            { event = "notify", find = "All parsers are up%-to%-date" },
+          },
+        },
+      },
+      {
+        -- Svelte LSP
+        opts = { lang = "svelte", skip = true },
+        filter = { event = "notify", find = "No information available" },
       },
     })
-
-    opts.views = {
-      cmdline_popup = {
-        position = { row = 10, col = "50%" },
-      },
-      popupmenu = {
-        position = { row = 13, col = "50%" },
-        size = {
-          width = 60,
-          max_height = 10,
-        },
-        border = {
-          style = "rounded",
-          padding = { 0, 1 },
-        },
-        win_options = {
-          winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
-        },
-      },
-    }
   end,
 }
--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -1,18 +1,38 @@
 return {
-  "supermaven-inc/supermaven-nvim",
-  event = "BufRead",
-  config = function()
-    require("supermaven-nvim").setup({
-      keymaps = {
-        accept_word = "<C-]>",
-        clear_suggestion = "<C-[>",
-      },
-      color = {
-        suggestion_color = "#414868",
-        cterm = 244,
-      },
-      disable_inline_completion = false, -- disables inline completion for use with cmp
-      disable_keymaps = false, -- disables built in keymaps for more manual control
-    })
-  end,
+  {
+    "supermaven-inc/supermaven-nvim",
+    event = "BufRead",
+    config = function()
+      require("supermaven-nvim").setup({
+        keymaps = {
+          accept_word = "<C-]>",
+          clear_suggestion = "<C-[>",
+        },
+        color = {
+          suggestion_color = "#414868",
+          cterm = 244,
+        },
+        disable_inline_completion = false, -- disables inline completion for use with cmp
+        disable_keymaps = false, -- disables built in keymaps for more manual control
+      })
+    end,
+  },
+  {
+    "folke/noice.nvim",
+    optional = true,
+    opts = function(_, opts)
+      vim.list_extend(opts.routes, {
+        {
+          filter = {
+            event = "msg_show",
+            any = {
+              { find = "Starting Supermaven" },
+              { find = "Supermaven Free Tier" },
+            },
+          },
+          skip = true,
+        },
+      })
+    end,
+  },
 }
