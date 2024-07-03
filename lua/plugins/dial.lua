9ff4fc5a7645f4347281a67f609726dba50450f9
ABDO <lonihilist@gmail.com>
2024-07-04 00:33:41 +0100

    fix(dial): not working after update

--- lua/plugins/dial.lua
+++ lua/plugins/dial.lua
@@ -9,7 +9,7 @@ function M.dial(increment, g)
   -- Use visual commands for VISUAL 'v', VISUAL LINE 'V' and VISUAL BLOCK '\22'
   local is_visual = mode == "v" or mode == "V" or mode == "\22"
   local func = (increment and "inc" or "dec") .. (g and "_g" or "_") .. (is_visual and "visual" or "normal")
-  local group = M.dials_by_ft[vim.bo.filetype] or "default"
+  local group = vim.g.dials_by_ft[vim.bo.filetype] or "default"
   return require("dial.map")[func](group)
 end
 
@@ -25,9 +25,5 @@ return {
     { "g<C-a>", function() return M.dial(true, true) end, expr = true, desc = "Increment", mode = {"n", "v"} },
     { "g<C-x>", function() return M.dial(false, true) end, expr = true, desc = "Decrement", mode = {"n", "v"} },
     }
-  end,
-  config = function(_, opts)
-    require("dial.config").augends:register_group(opts.groups)
-    M.dials_by_ft = opts.dials_by_ft
   end,
 }
