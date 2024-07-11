a347b0b3881568529d3bdc9a179c461daeb47a54
ABDO <lonihilist@gmail.com>
2024-07-11 01:29:58 +0100

    feat: update global keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,12 +6,20 @@
 local function map(mode, lhs, rhs, opts)
   opts = opts or {}
   opts.silent = opts.silent ~= false
-  -- opts.noremap = opts.noremap ~= false
+  opts.noremap = opts.noremap ~= false
   vim.keymap.set(mode, lhs, rhs, opts)
 end
 
 -- Quit
 map({ "n" }, "<C-q>", "<cmd>qa<cr>", { desc = "Quit all" })
+
+-- Better Search navigation
+map("n", "n", "nzz")
+map("n", "N", "Nzz")
+map("n", "*", "*zz")
+map("n", "#", "#zz")
+map("n", "g*", "g*zz")
+map("n", "g#", "g#zz")
 
 -- Press jk fast to enter normal mode
 map("i", "jk", "<ESC>")
@@ -24,28 +32,34 @@ map("n", "<leader>O", "m`O<esc>``", { desc = "Insert blank line below" })
 map({ "i", "n", "v" }, "<C-a>", "<esc>ggVG")
 
 -- Move to start/end of line
-map({ "n", "x", "o" }, "H", "g^")
-map({ "n", "x", "o" }, "L", "g$")
+map({ "n", "x" }, "H", "^")
+map({ "n", "x" }, "L", "$")
 
 -- Delete word with backspace
 map("i", "<C-BS>", "<C-w>")
+map("i", "<C-Del>", "<esc>cw")
 
 map("n", "<cr>", "ciw", { desc = "Change word under cursor" })
 map("n", "gl", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+
+-- windows
+map("n", "<leader>wq", "<C-W>c", { desc = "Delete Window", remap = true })
+map("n", "<leader>ws", "<C-W>s", { desc = "Split Window Below", remap = true })
+map("n", "<leader>wv", "<C-W>v", { desc = "Split Window Right", remap = true })
+
+-- Comment
+vim.keymap.del("n", "<c-/>") -- Remove terminal keybind
+map("n", "<c-/>", "gcc", { desc = "comment toggle", remap = true })
+map("v", "<c-/>", "gc", { desc = "comment toggle", remap = true })
+
+-- Terminal
+-- stylua: ignore
+local lazyterm = function() LazyVim.terminal(nil, { cwd = LazyVim.root() }) end
+map("n", "<c-\\>", lazyterm, { desc = "Terminal (Root Dir)" })
+map("t", "<c-\\>", "<cmd>close<cr>", { desc = "Hide Terminal" })
 
 -- Google search
 local searching_google_in_visual =
   [[<ESC>gv"gy<ESC>:lua vim.fn.system({'xdg-open', 'https://google.com/search?q=' .. vim.fn.getreg('g')})<CR>]]
 
 vim.keymap.set("v", "gx", searching_google_in_visual, { silent = true, noremap = true })
-
-local Util = require("lazyvim.util")
--- local unmap = vim.keymap.del
-
-if not vim.g.vscode then
-  map("n", "<C-\\>", function()
-    Util.terminal(nil, { cwd = Util.root() })
-  end, { desc = "Terminal (root dir)" })
-
-  map("t", "<C-\\>", "<cmd>close<cr>", { desc = "Hide Terminal" })
-end
