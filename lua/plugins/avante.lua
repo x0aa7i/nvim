aef53963897eac2f42e01ed06d222cbc4ba2f87d
ABDO <lonihilist@gmail.com>
2024-09-01 10:06:56 +0100

    feat: avante

---
+++ lua/plugins/avante.lua
@@ -0,0 +1,51 @@
+return {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  -- build = "make", -- This is Optional, only if you want to use tiktoken_core to calculate tokens count
+  dependencies = {
+    "HakonHarnes/img-clip.nvim",
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    --- The below is optional, make sure to setup it properly if you have lazy=true
+    {
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
+  opts = {
+    provider = "groq",
+    vendors = {
+      groq = {
+        endpoint = "https://api.groq.com/openai/v1/chat/completions",
+        model = "llama-3.1-70b-versatile",
+        api_key_name = "GROQ_API_KEY",
+        --- this function below will be used to parse in cURL arguments.
+        parse_curl_args = function(opts, code_opts)
+          return {
+            url = opts.endpoint,
+            headers = {
+              ["Accept"] = "application/json",
+              ["Content-Type"] = "application/json",
+              ["Authorization"] = "Bearer " .. os.getenv(opts.api_key_name),
+            },
+            body = {
+              model = opts.model,
+              messages = require("avante.providers").openai.parse_message(code_opts), -- you can make your own message, but this is very advanced
+              temperature = 0,
+              max_tokens = 4096,
+              stream = true, -- this will be set by default.
+            },
+          }
+        end,
+        -- The below function is used if the vendors has specific SSE spec that is not claude or openai.
+        parse_response_data = function(data_stream, event_state, opts)
+          require("avante.providers").openai.parse_response(data_stream, event_state, opts)
+        end,
+      },
+    },
+  },
+}
