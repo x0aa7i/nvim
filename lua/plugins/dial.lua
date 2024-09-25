0e9a0dbe5550c747bc2495912ee9f99bbd05e31a
ABDO <lonihilist@gmail.com>
2024-09-25 21:46:29 +0100

    fix: use dial default keymaps

--- lua/plugins/dial.lua
+++ lua/plugins/dial.lua
@@ -1,29 +1,14 @@
-local M = {}
----@type table<string, table<string, string[]>>
-M.dials_by_ft = {}
-
----@param increment boolean
----@param g? boolean
-function M.dial(increment, g)
-  local mode = vim.fn.mode(true)
-  -- Use visual commands for VISUAL 'v', VISUAL LINE 'V' and VISUAL BLOCK '\22'
-  local is_visual = mode == "v" or mode == "V" or mode == "\22"
-  local func = (increment and "inc" or "dec") .. (g and "_g" or "_") .. (is_visual and "visual" or "normal")
-  local group = vim.g.dials_by_ft[vim.bo.filetype] or "default"
-  return require("dial.map")[func](group)
-end
-
 return {
   "monaqa/dial.nvim",
-  keys = function()
-    return {
-    -- stylua: ignore start
-    { "<leader>ci", function() return M.dial(true) end, expr = true, desc = "Increment", mode = {"n", "v"} },
-    { "<leader>cd", function() return M.dial(false) end, expr = true, desc = "Decrement", mode = {"n", "v"} },
+  opts = function(_, opts)
+    local augend = require("dial.augend")
 
-    { "<C-x>", function() return M.dial(false) end, expr = true, desc = "Decrement", mode = {"n", "v"} },
-    { "g<C-a>", function() return M.dial(true, true) end, expr = true, desc = "Increment", mode = {"n", "v"} },
-    { "g<C-x>", function() return M.dial(false, true) end, expr = true, desc = "Decrement", mode = {"n", "v"} },
-    }
+    return vim.tbl_deep_extend("force", opts, {
+      groups = {
+        default = {
+          augend.constant.alias.bool, -- boolean value (true <-> false)
+        },
+      },
+    })
   end,
 }
