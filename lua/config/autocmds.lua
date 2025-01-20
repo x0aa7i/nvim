72a7a563acd925acee81ae35315821410edb33f4
ABDO <lonihilist@gmail.com>
2025-01-20 11:55:56 +0100

    fix: remove '-' from iskeyword

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,10 +2,19 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+-- Disable diagnostic for .env files
 vim.api.nvim_create_autocmd("BufReadPost", {
-  pattern = "*.env",
+  pattern = { "*.env", ".env.*" },
   desc = "Disable diagnostic for .env files",
   callback = function(ev)
     vim.diagnostic.enable(false, { bufnr = ev.buf })
+  end,
+})
+
+-- Remove '-' from iskeyword
+vim.api.nvim_create_autocmd({ "BufEnter", "BufReadPost" }, {
+  pattern = "*",
+  callback = function()
+    vim.opt_local.iskeyword:remove("-")
   end,
 })
