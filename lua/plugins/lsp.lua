956a8bbfa1227fd7d995ff8ada8918a0ab20d4f6
ABDO <lonihilist@gmail.com>
2025-01-21 13:07:03 +0100

    feat: harper-ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,6 +7,25 @@ local ts_filetypes = {
   "svelte",
 }
 
+---Retrieves an LSP client by name
+---@param name string
+---@return vim.lsp.Client | nil
+local function getLSPClient(name)
+  return vim.lsp.get_clients({ bufnr = 0, name = name })[1]
+end
+
+---Toggles an LSP client by name
+---@param name string
+---@return nil
+local function toggleLSPClient(name)
+  local client = getLSPClient(name)
+  if client then
+    client.stop(true)
+  else
+    vim.cmd("LspStart " .. name)
+  end
+end
+
 return {
   {
     "neovim/nvim-lspconfig",
@@ -47,6 +66,7 @@ return {
           },
         },
       }
+
       opts.servers.tailwindcss = {
         filetypes_exclude = { "markdown" },
         filetypes_include = {},
@@ -63,6 +83,37 @@ return {
           },
         },
       }
+
+      opts.servers.harper_ls = {
+        autostart = false,
+        settings = {
+          ["harper-ls"] = {
+            userDictPath = vim.fn.stdpath("config") .. "/spell/en.utf-8.add",
+            -- fileDictPath = "~/.config/harper/dictionaries/files/",
+            codeActions = { forceStable = true },
+          },
+        },
+      }
+
+      opts.setup = opts.setup or {}
+      opts.setup.harper_ls = function()
+        Snacks.toggle({
+          name = "Grammar Checker",
+          get = function()
+            return getLSPClient("harper_ls") ~= nil
+          end,
+          set = function()
+            toggleLSPClient("harper_ls")
+          end,
+        }):map("<leader>clg")
+      end
+
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      keys[#keys + 1] = { "<leader>cli", "<cmd>LspInfo<cr>", desc = "Info" }
+
+      require("which-key").add({
+        { "<leader>cl", group = "LSP", icon = LazyVim.config.icons.diagnostics.Info },
+      })
     end,
   },
   {
@@ -79,6 +130,7 @@ return {
         "tailwindcss-language-server",
         "css-lsp",
         "json-lsp",
+        "harper-ls", -- grammar check
         "stylelint-lsp", -- css linter
         "emmet-language-server", -- css/html completions
         "lua-language-server",
